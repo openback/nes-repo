73fe9744264436b574df9c6aa0def82f7164eb24
Timothy Caraballo <openback@gmail.com>
2016-03-17 14:16:32 -0400

    Nicer game data parsing

--- lib/gameparser.rb
+++ lib/gameparser.rb
@@ -2,6 +2,9 @@ require_relative './gamecollection'
 require_relative './game'
 
 class GameParser
+  QUOTED_REGEX = lambda { |name| /\s+#{name}\s+"(.*?)"/m }
+  UNQUOTED_REGEX = lambda { |name| /\s+#{name}\s+(\w*)/m }
+
   def self.parse stream, maintainer, version=1
     raise ArgumentError.new('Must pass an IO stream') unless stream.respond_to? :gets
     raise ArgumentError.new('Invalid Dat file') if stream.gets !~ /clrmamepro(\s*)?\(/
@@ -26,23 +29,20 @@ class GameParser
   end
 
   def self.parse_game dat, maintainer, version=1
-    Game.new({
-      :name => self.get_quoted_val(dat, 'name'),
-      :description => self.get_quoted_val(dat, 'description'),
-      :maintainer => maintainer,
-      :md5 => self.get_val(dat, 'md5'),
-      :sha1 => self.get_val(dat, 'sha1'),
-      :crc => self.get_val(dat, 'crc'),
-      :version => version
-    })
+    unquoted_vals = [:md5, :sha1, :crc]
+    quoted_vals = [:name, :description]
+
+    Game.new(
+      {
+        :maintainer => maintainer,
+        :version => version
+      }.merge(self.collect(dat, unquoted_vals, UNQUOTED_REGEX))
+      .merge(self.collect(dat, quoted_vals, QUOTED_REGEX))
+    )
   end
 
   private
-  def self.get_quoted_val dat, name
-    /\s+#{name}\s+"(.*?)"/m.match(dat).captures[0]
-  end
-
-  def self.get_val dat, name
-    /\s+#{name}\s+(\w*)/m.match(dat).captures[0]
+  def self.collect dat, names, regex
+      Hash[names.collect { |name| [name, regex.call(name.to_s).match(dat).captures[0]] }]
   end
 end
